6868044409aa929390fed3942d57dbd25153fb6e
Oscar GG <torrente76@yahoo.es>
2014-12-18 11:26:18 +0100

    Creación del paquete

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='utilidades',
+      version='0.1',
+      description='Utilidades de uso general',
+      url='https://github.com/oogg06/prueba-utils-pyt',
+      author='ogg06',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['utilidades'],
+      zip_safe=False)
---
+++ utilidades/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+
+UTILIDADES_OK=0
+def descargar_archivo(url, nombre_archivo_destino):
+    return UTILIDADES_OK
+
+
